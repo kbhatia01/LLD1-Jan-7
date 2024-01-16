9e8cc8a6e0e3b979e3b80767761b481e1dba1d5a
Kbhatia01 <karanbhatiacs@gmail.com>
2024-01-16 22:08:22 +0530

    add thread pools

--- src/introToThreads/Main.java
+++ src/introToThreads/Main.java
@@ -1,5 +1,9 @@
 package introToThreads;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
 //        HelloWorldPrinter hw = new HelloWorldPrinter();
@@ -12,6 +16,18 @@ public class Main {
             Runnable pn = new PrintNumber(i);
             Thread t1 = new Thread(pn);
             t1.start();
+        }
+
+
+        ExecutorService es = Executors.newFixedThreadPool(-1);
+
+//        ExecutorService es = Executors.newCachedThreadPool();
+        for(int i=0; i<=100000; i++){
+            if(i==80){
+                System.out.println("hey");
+            }
+            Runnable pn = new PrintNumber(i);
+            es.execute(pn);
         }
     }
 
